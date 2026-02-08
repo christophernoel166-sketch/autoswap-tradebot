b1bdac95fa2935ae034092928e5644ebb38e6d3c
christophernoel166-sketch <christophernoel166@gmail.com>
2026-02-08 05:02:01 -0800

    fix(solana): rename sendSol export to sendSolToUser

--- src/solana/sendSol.js
+++ src/solana/sendSol.js
@@ -5,11 +5,9 @@ import {
   LAMPORTS_PER_SOL,
 } from "@solana/web3.js";
 import { INTERNAL_TRADING_WALLET } from "./internalWallet.js";
-
 import { getConnection } from "../utils/solanaConnection.js";
 
-export async function sendSol({ to, amountSol }) {
-
+export async function sendSolToUser({ to, amountSol }) {
   const connection = getConnection();
 
   const lamports = Math.floor(amountSol * LAMPORTS_PER_SOL);
