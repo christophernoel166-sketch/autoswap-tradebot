3a1ce5e8a554d55a089b4155d7b3139767692b1b
Christopher Noel <christophernoel166@gmail.com>
2026-04-20 21:54:08 +0600

    Update chart analysis route

--- src/api/routes/chartAnalysis.js
+++ src/api/routes/chartAnalysis.js
@@ -1,5 +1,5 @@
 import express from "express";
-import User from "../../models/User.js";
+import User from "../../../models/User.js";
 import { analyzeChartEntry } from "../../services/chartEntryService.js";
 
 const router = express.Router();
