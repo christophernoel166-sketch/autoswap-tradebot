f62afa0b44f2d7208f12fd82b38104ae874b446b
Christopher Noel <christophernoel166@gmail.com>
2026-05-22 00:56:31 +0600

    Refine token routes

--- src/api/routes/tokens.js
+++ src/api/routes/tokens.js
@@ -414,7 +414,7 @@ try {
 
 
 const refreshedTokens = await Promise.all(
-  cachedTokens.slice(0, 100).map(async (token) => {
+  cachedTokens.slice(0, 70).map(async (token) => {
     try {
       const market = await fetchTokenMarketData(token.mintAddress);
 
