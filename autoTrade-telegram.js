fc61ebbfb815fb96f1ff4b455997eb31522b7fda
Christopher Noel <christophernoel166@gmail.com>
2026-06-01 21:14:17 +0600

    Refine auto trade bot logic

--- autoTrade-telegram.js
+++ autoTrade-telegram.js
@@ -2809,7 +2809,12 @@ LOG.info(
   "🧪 Redis active wallets"
 );
 
-
+LOG.warn(
+  {
+    walletKeysCount: walletKeys.length,
+  },
+  "🧪 SHOULD BLOCKCHAIN RECOVERY RUN?"
+);
 
 // REBUILD FROM BLOCKCHAIN
 if (!walletKeys.length) {
