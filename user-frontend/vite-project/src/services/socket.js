ebe3a96316fce8119e8e522e2a9d6e97f2b4e27c
Christopher Noel <christophernoel166@gmail.com>
2026-07-04 23:50:26 +0600

    Update frontend socket service

--- user-frontend/vite-project/src/services/socket.js
+++ user-frontend/vite-project/src/services/socket.js
@@ -29,7 +29,7 @@ export function connectSocket(walletAddress) {
 
   const backend =
   import.meta.env.VITE_API_URL ||
-  "https://autoswap-tradebot-production-9c06.up.railway.app";
+  "https://autoswap-tradebot-production.up.railway.app";
 
   socket = io(backend, {
     transports: ["websocket"],
